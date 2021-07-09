a1b625c3ff3c4c7ccfcf25ead9b1f9ef3b9459de
Purna Gurung <purnagurung903@gmail.com>
2021-07-09 12:09:56 -0400

    added emailItems and fixed compose button

---
+++ gmail-clone/src/temp/EmailData.js
@@ -0,0 +1,24 @@
+export const emailData = [
+  {
+    starred: true,
+    from: 'Purna',
+    subject: 'Yoooo',
+    message: 'Thikai Xa Niroj dai',
+    received: '08:20',
+    read: true
+}, {
+    starred: true,
+    from: 'Niroj',
+    subject: 'Yoooo',
+    message: ' k Xa ktaho ',
+    received: '08:30',
+    read: true
+}, {
+    starred: false,
+    from: 'Anish',
+    subject: 'Yoooo',
+    message: 'sup breh',
+    received: '09:25',
+    read: false
+},
+]
